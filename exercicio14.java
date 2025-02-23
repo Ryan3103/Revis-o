40e2042baed1798ede802df0c53c9d3789ca9b60
Ryan Gabriel <ryangabrielsilvag052@gmail.com>
2025-02-22 21:12:03 -0300

    Add files via upload

---
+++ exercicio14.java
@@ -0,0 +1,26 @@
+package Revisao;
+import java.util.Scanner;
+
+public class exercicio14 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int[][] matriz = new int[3][3];
+            int somaDiagonalPrincipal = 0;
+
+            System.out.println("Digite os elementos da matriz 3x3:");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.printf("Elemento [%d,%d]: ", i + 1, j + 1);
+                    matriz[i][j] = scanner.nextInt();
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                somaDiagonalPrincipal += matriz[i][i];
+            }
+
+            System.out.println("\nSoma da diagonal principal: " + somaDiagonalPrincipal);
+            scanner.close();
+        }
+    }
---
+++ exercicio15.java
@@ -0,0 +1,26 @@
+package Revisao;
+import java.util.Scanner;
+public class exercicio15 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int[][] matriz = new int[3][3];
+            int somaDiagonalSecundaria = 0;
+
+            System.out.println("Digite os elementos da matriz 3x3:");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.printf("Elemento [%d,%d]: ", i + 1, j + 1);
+                    matriz[i][j] = scanner.nextInt();
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                somaDiagonalSecundaria += matriz[i][2 - i];
+            }
+
+            System.out.println("\nSoma da diagonal secundária: " + somaDiagonalSecundaria);
+            scanner.close();
+        }
+    }
+
